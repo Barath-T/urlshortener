a545228e5a6c1f2c28161957dcd22cf7e8bae084
nktp <narenkarthicktp@gmail.com>
2025-02-02 23:02:46 +0530

    chore: added sample .env
    - example usage of `database_handler.rs` in `main.rs`
    - mogodb variables listed in `.env`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+pub mod database_handler;
+pub mod url_data;
+
+use std::env;
+use dotenv::dotenv;
+use mongodb::error;
+
+use database_handler::UrlCollection;
+
+
+fn get_env_vars(var_name: &str) -> String {
+	env::var(var_name).expect("{var_name} must be set")
 }
+
+#[tokio::main]
+async fn main() -> error::Result<()> {
+
+	dotenv().ok();
+	let uri = get_env_vars("MONGO_URI");
+	let db_name = get_env_vars("MONGO_DB_NAME");
+	let collection_name = get_env_vars("MONGO_COLLECTION_NAME");
+
+	let x = UrlCollection::new(uri.as_str(), db_name.as_str(), collection_name.as_str()).await?;
+
+	if let Some(mut y) = x.get_url_data(2).await? {
+		println!("{:?}", y);
+		y.original_url = "youtube.com".to_string();
+		x.modify_url_data(2, &y).await?;
+		println!("{:?}", y);
+	}
+
+	Ok(())
+}
+
